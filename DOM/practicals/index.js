2084dc55ee44007f5a714cf7075507c5dd58ad02
Umar <umarmunafahmed@gmail.com>
2026-01-07 23:40:21 +0530

    DOM started

---
+++ DOM/practicals/index.js
@@ -0,0 +1,24 @@
+/* 
+Accessing Elements: Methods like getElementById() (selects by id),
+                                    getElementsByClassName() (selects by class),
+                                    getElementsByTagName() (selects by tag name), 
+                                    and querySelector()/querySelectorAll() 
+                                    (selects using CSS selectors).
+
+
+Manipulating Content: 
+Properties such as textContent (gets or sets text), 
+innerHTML (gets or sets HTML content), and style (sets CSS styles).
+
+
+Modifying Structure: Methods like createElement() (creates new elements), 
+appendChild() (adds child nodes), and remove() (removes elements).
+
+
+Event Handling: 
+The addEventListener() method attaches event handlers (like "click") to elements. */
+
+
+
+//document.getElementById("head").innerHTML ="lets goo!";
+//document.getElementById("head").style.color="red";
