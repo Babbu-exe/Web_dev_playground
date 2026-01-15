100dad752e299090bf87a40fbcd1ca22de18a658
Umar <umarmunafahmed@gmail.com>
2026-01-15 20:14:42 +0530

    button added

--- Dice game/dice.js
+++ Dice game/dice.js
@@ -1,3 +1,5 @@
+function rollDice() {
+
 // Generate a random number between 1 and 6 for Player 1
 var count1 = Math.floor(Math.random() * 6) + 1; 
 console.log(count1); // Log the result of Player 1's roll
@@ -17,11 +19,12 @@ var img2 = document.querySelectorAll("img")[1].setAttribute("src", imgsrc);
 // Compare the results of Player 1 and Player 2
 if (count1 > count2) {
     // Update the header to declare Player 1 as the winner
-    document.querySelector("h1").textContent = "Player 1 Wins! 🚩"; 
+    document.querySelector("h4").textContent = "Babbu Wins! 🚩"; 
 } else if (count2 > count1) {
     // Update the header to declare Player 2 as the winner
-    document.querySelector("h1").textContent = "Player 2 Wins! 🚩"; 
+    document.querySelector("h4").textContent = "sadi Wins! 🚩"; 
 } else {
     // Update the header to declare a draw if both players roll the same number
-    document.querySelector("h1").textContent = "It's a Draw!"; 
+    document.querySelector("h4").textContent = "It's a Draw!"; 
+}
 }
